1dc59d82abdec0c0d1e4d97fbe11a89bc6682b4e
Anilk1228 <bhukyaanil2013@gmail.com>
2025-05-11 10:01:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
+api_id = "28413721"
+api_hash = "c353284aee7154723e9766470434cf0a"
 bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+auth_users = [1860169540]
