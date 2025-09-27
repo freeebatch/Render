2f2ed2f1be7cab72df719932918248f1f2b9e742
freeebatch <koushalanurag0@gmail.com>
2025-09-27 10:39:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "28413721"
 api_hash = "c353284aee7154723e9766470434cf0a"
-bot_token = "6393100057:AAFgUrLELyXbWFCWpGqR7WzVxWtba4nmJAA"
+bot_token = "8037871175:AAFVJo_EZhnEnGqAB21UHX_2XJKOrePGmDM"
 auth_users = [1671836568]
